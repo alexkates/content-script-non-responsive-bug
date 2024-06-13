a645e153b8aa02bf75f58bdfac9e788d510739d1
Alex Kates <alex.kates@croissant.com>
2024-06-13 18:23:28 -0400

    chore: Update browser API usage in content script

--- content-script-non-responsive-bug Extension/Resources/background.js
+++ content-script-non-responsive-bug Extension/Resources/background.js
@@ -1,3 +1,6 @@
+browser.runtime.onMessage.addListener(handleMessage);
+browser.tabs.onActivated.addListener(handleActivated);
+
 async function fetchRandomJSONData() {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
 
@@ -5,16 +8,12 @@ async function fetchRandomJSONData() {
 }
 
 function handleMessage(request, sender, sendResponse) {
-  console.log(new Date().toISOString());
+  console.log(`Received message from content script: ${request} from tab: ${sender.tab.url}`);
 
   return fetchRandomJSONData();
 }
 
-browser.runtime.onMessage.addListener(handleMessage);
-
 function handleActivated(activeInfo) {
-  console.log(`Tab ${activeInfo.tabId} was activated`);
+  console.log(`Tab activated: ${activeInfo.tabId}`);
   browser.tabs.sendMessage(activeInfo.tabId, "Tab Activated!");
 }
-
-browser.tabs.onActivated.addListener(handleActivated);
--- content-script-non-responsive-bug Extension/Resources/content.js
+++ content-script-non-responsive-bug Extension/Resources/content.js
@@ -1,15 +1,15 @@
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(`${new Date().toISOString()}: ${message}`);
+});
+
 async function sendMessageAndHandleResponse() {
     try {
+        console.log(`${new Date().toISOString()}: Sending message to background script ...`);
         const randomJSONData = await browser.runtime.sendMessage("Ping");
-
-        console.log(`${new Date().toISOString()}: ${JSON.stringify(randomJSONData)}`);
+        console.log(`${new Date().toISOString()}: Received response from background script ${JSON.stringify(randomJSONData)}`);
     } catch (e) {
         console.log(e);
     }
 }
 
 setInterval(sendMessageAndHandleResponse, 1000);
-
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(`${new Date().toISOString()}: ${message}`);
-});
